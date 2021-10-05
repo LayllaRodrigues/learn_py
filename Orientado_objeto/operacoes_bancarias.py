de53229b762114fe1ed8158d0cab40750d16278b
laylla.rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2021-10-05 14:16:16 -0300

    first commit

---
+++ Orientado_objeto/operacoes_bancarias.py
@@ -0,0 +1,8 @@
+from conta_bank import cria_conta, deposita, saca, extrato
+
+conta = cria_conta(123, "Nico", 55.0, 1000.0)
+deposita(conta, 300.0)
+extrato(conta)
+saca(conta, 100.0)
+extrato(conta)
+ 
